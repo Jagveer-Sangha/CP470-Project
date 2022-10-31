f5a004c1245d6bee06f5ff3ded6f1f44f399f75e
jagveer-sangha <jagveersangha@gmail.com>
2022-10-31 17:31:51 -0400

    Fixed motivation page naming and logic

--- app/src/main/java/com/example/project/Motivation.java
+++ app/src/main/java/com/example/project/Motivation.java
@@ -1,7 +1,6 @@
-package com.example.androidproject;
+package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,8 +8,8 @@ import android.widget.TextView;
 
 public class Motivation extends AppCompatActivity {
 
-    private Button motiButton;
-    private TextView motiText;
+    private Button generateQuote;
+    private TextView quotesView;
 
 
     private static final String ACTIVITY_NAME = "Motivation";
@@ -20,8 +19,8 @@ public class Motivation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motivation);
 
-        motiText = findViewById(R.id.textView);
-        motiButton = findViewById(R.id.motivation);
+        quotesView = findViewById(R.id.textView);
+        generateQuote = findViewById(R.id.motivation);
 
         String[] quotes = {"Do not save what is left after spending, but spend what is left after saving.",
                             "Never spend your money before you have it.",
@@ -30,11 +29,12 @@ public class Motivation extends AppCompatActivity {
 
 
 
-        motiButton.setOnClickListener(new View.OnClickListener() {
+        generateQuote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int i = (int) (Math.random()*4);
-                motiText.setText(quotes[i]);
+
+                quotesView.setText(quotes[i]);
             }
         });
 
