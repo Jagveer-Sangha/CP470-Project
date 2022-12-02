b105a03a0a771967827b194a63e2cfa508a303b9
safi1610 <safi1610@mylaurier.ca>
2022-12-01 22:19:44 -0500

    DataBase for goal inputs is now functioning and can save entered goal values

--- app/src/main/java/com/example/project/GoalActivity.java
+++ app/src/main/java/com/example/project/GoalActivity.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +27,10 @@ import java.util.ArrayList;
 public class GoalActivity extends AppCompatActivity {
 
     ArrayList <String> goalsListStrg = new ArrayList<>();
+    private static final String ACTIVITY_NAME = "GoalsWindow";
+
+    GoalsDatabaseHelper goalsDB;
+    SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +41,28 @@ public class GoalActivity extends AppCompatActivity {
         EditText inputBox = findViewById(R.id.inputBox);
         Button goalEnter = findViewById(R.id.goalsenter);
         ListView goalsList = findViewById(R.id.goalsList);
+        goalsDB = new GoalsDatabaseHelper(this);
+        database = goalsDB.getWritableDatabase();
 
 
         GoalAdapter goalAdapter = new GoalAdapter(this);
         goalsList.setAdapter(goalAdapter);
+
+        Cursor c = database.rawQuery("select * from "+ GoalsDatabaseHelper.TABLE_OF_GOALS_ITEMS + ";", null);
+        c.moveToFirst();
+
+        while(!c.isAfterLast()){
+            String str = c.getString((c.getColumnIndexOrThrow(GoalsDatabaseHelper.KEY_MESSAGE)));
+            goalsListStrg.add(str);
+            Log.i(ACTIVITY_NAME, "SQL MESSAGE: " + c.getString(c.getColumnIndexOrThrow(GoalsDatabaseHelper.KEY_MESSAGE)));
+            Log.i(ACTIVITY_NAME, "Cursor Column Count =" + c.getColumnCount());
+            c.moveToNext();
+        }
+
+        for(int i = 0; i < c.getColumnCount(); i++){
+            c.getColumnName(i);
+            System.out.println(c.getColumnName(i));
+        }
 
         goalEnter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,11 +70,27 @@ public class GoalActivity extends AppCompatActivity {
                 String goalVal = inputBox.getText().toString();
                 goalsListStrg.add(goalVal);
 
+                ContentValues val = new ContentValues();
+                val.put(GoalsDatabaseHelper.KEY_MESSAGE, inputBox.getText().toString());
+                long insertId = database.insert(GoalsDatabaseHelper.TABLE_OF_GOALS_ITEMS, null, val);
+                Cursor c2 = database.query(GoalsDatabaseHelper.TABLE_OF_GOALS_ITEMS, null, GoalsDatabaseHelper.KEY_ID + "=" + insertId, null, null, null, null);
+                c2.moveToFirst();
+                c2.close();
+
                 goalAdapter.notifyDataSetChanged();
                 inputBox.setText("");
 
             }
+
+
         });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        goalsDB.close();
+        super.onDestroy();
     }
 
     private class GoalAdapter extends ArrayAdapter<String>{
