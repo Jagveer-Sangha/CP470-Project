e0f83905a8adbfe7d8ce3b5c3469dfb1f2478453
TeeTran101 <tran9810@mylaurier.ca>
2022-12-06 15:45:06 -0500

    fixed toast

--- app/src/main/java/com/example/project/Motivation.java
+++ app/src/main/java/com/example/project/Motivation.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Motivation extends AppCompatActivity {
 
